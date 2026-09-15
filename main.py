67e0048ca9c331fb69cc334e75e9fb1295f8bf6e
daksh <dakshraheja13@gmail.com>
2026-09-15 22:47:07 +0530

    initialising

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import ollama
+app=FastAPI()
+@app.post("/generate")
+def generate (prompt:str):
+    response=ollama.chat(model="mistral",messages=[{"role":"user","content":prompt}])
+    return {"response":response["message"]}
+
