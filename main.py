32b3d28978cbf57e08e48fd77834fb77007642c1
daksh <dakshraheja13@gmail.com>
2026-09-16 14:21:06 +0530

    testcommit

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app=FastAPI()
 def generate (prompt:str):
     response=ollama.chat(model="mistral",messages=[{"role":"user","content":prompt}])
     return {"response":response["message"]}
-
+print( generate("give me the code to print 1 to 100 using for loop python"))
